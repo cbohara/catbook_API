0f3c4e29d19c32bfa1fa9d58efbae5a5dfa908e8
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-12 13:46:06 -0700

    setting up model relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161012202201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_hobbies", force: :cascade do |t|
+  end
+
+  create_table "cats", force: :cascade do |t|
+    t.string   "name"
+    t.string   "breed"
+    t.string   "weight"
+    t.string   "temperament"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
